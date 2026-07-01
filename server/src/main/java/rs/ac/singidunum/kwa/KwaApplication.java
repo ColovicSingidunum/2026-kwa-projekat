e577644ca7274d8355f408e4c0f1bb6a29ce5158
Александар Чоловић <aleksandar.colovic.23@singimail.rs>
2026-07-01 19:33:58 +0200

    feat(server): postavi Spring Boot backend

---
+++ server/src/main/java/rs/ac/singidunum/kwa/KwaApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.kwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/rs/ac/singidunum/kwa/KwaApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.kwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
